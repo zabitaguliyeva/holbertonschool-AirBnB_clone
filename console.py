d50351d9af2ae577f7b1d4aaad734127263106b0
Elchin <elcinceferli@gmail.com>
2024-02-29 20:42:30 +0400

    fix bug

--- console.py
+++ console.py
@@ -82,28 +82,32 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         comand = arg.split()
-        if len(comand) == 0:
-            print("** class name missing **")
-        elif comand[0] not in self.__classes:
-            print("** class doesn't exist **")
-        elif len(comand) == 1:
-            print("** instance id missing **")
-        elif len(comand) == 2:
+        if len(comand) == 4:
+            data = storage.all()
+            key = "{}.{}".format(comand[0], comand[1])
+            if (key not in storage.all()):
+                print("** no instance found **")
+            else:
+                for k, v in data.items():
+                    if comand[1] == k.split('.')[1]:
+                        setattr(v, comand[2], comand[3].split("\"")[1])
+                        storage.save()
+
+        if len(comand) == 3:
+            print("** value missing **")
+
+        if len(comand) == 2:
             key = "{}.{}".format(comand[0], comand[1])
             if (key not in storage.all()):
                 print("** no instance found **")
             else:
                 print("** attribute name missing **")
-        elif len(comand) == 3:
-            print("** value missing **")
-        else:
-            key = comand[2]
-            value = comand[3]
-            data = storage.all()
-            for k, v in data.items():
-                if comand[1] == k.split('.')[1]:
-                    setattr(v, key, value.split("\"")[1])
-                    storage.save()
+
+        if len(comand) == 1:
+            print("** instance id missing **")
+
+        if len(comand) == 0:
+            print("** class name missing **")
 
 
 if __name__ == "__main__":
