cf297d52dd26395e359c97bb595f650bea580ae3
Elchin <elcinceferli@gmail.com>
2024-03-01 18:14:47 +0400

    make tests dir

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+import unittest
+from models.base_model import BaseModel
+from models import storage
+
+class TestBaseModel(unittest.TestCase):
+    def test_save(self):
+       instance = BaseModel()
+       instance.save()
+       self.assertIn("BaseModel." + instance.id, storage.all())
+
+if __name__ == "__main__":
+    unittest.main()
