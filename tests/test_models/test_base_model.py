f808657dbcf8c9469976f75fbc6011b2f5bec6ef
Elchin <elcinceferli@gmail.com>
2024-03-01 20:44:59 +0400

    baseclass save test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,12 @@
 import unittest
 from models.base_model import BaseModel
 from models import storage
+import json
 
 class TestBaseModel(unittest.TestCase):
     def test_save(self):
        instance = BaseModel()
        instance.save()
-       self.assertIn("BaseModel." + instance.id, storage.all())
+       with open("file.json", "r") as f:
+           data = json.load(f)
+           self.assertIn("BaseModel." + instance.id, data)
