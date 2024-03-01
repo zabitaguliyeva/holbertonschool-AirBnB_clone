143967701e685d49e1d6fbc18e7b3ecda856f3e7
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-01 13:32:35 +0300

    Created User class and uptade console.py to allow show, create, destroy, update and all used with User

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 from models.base_model import BaseModel
+""" class User that inherits from BaseModel:"""
 
 class User(BaseModel):
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    """Class User"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
