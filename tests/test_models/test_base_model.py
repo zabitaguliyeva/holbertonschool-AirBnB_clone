2a6d882d7dcf16ed19bd7b91a5ead1443a7e0f2f
Elchin <elcinceferli@gmail.com>
2024-03-01 21:08:57 +0400

    add to_dict test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,7 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import json
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     def test_save(self):
@@ -10,3 +11,10 @@ class TestBaseModel(unittest.TestCase):
        with open("file.json", "r") as f:
            data = json.load(f)
            self.assertIn("BaseModel." + instance.id, data)
+
+    def test_todict(self):
+        instance = BaseModel()
+        new_dict = instance.to_dict()
+        self.assertTrue(new_dict)
+        
+        
