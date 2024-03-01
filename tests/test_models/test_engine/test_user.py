85dbb2dac43ab7891de149ff3a5cd967d12ec31f
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-02 00:23:46 +0400

    test for User

--- tests/test_models/test_engine/test_user.py
+++ tests/test_models/test_engine/test_user.py
@@ -20,3 +20,29 @@ class TestUser(unittest.Testcase):
         user_model.email = "7644@holbertonstudents.com"
         self.assertEqual(user_model.email, "7644@holbertonstudents.com")
 
+    def test_type(self):
+        self.assertIsInstance(User.email, str)
+        self.assertIsInstance(User.password, str)
+        self.assertIsInstance(User.first_name, str)
+        self.assertIsInstance(User.last_name, str)
+
+    def test_email(self):
+        user = User()
+        user.email = "test@example.com"
+        self.assertEqual(user.email, "test@example.com")
+
+    def test_password(self):
+        user = User()
+        user.password = "password123"
+        self.assertEqual(user.password, "password123")
+
+    def test_first_name(self):
+        user = User()
+        user.first_name = "John"
+        self.assertEqual(user.first_name, "John")
+
+    def test_last_name(self):
+        user = User()
+        user.last_name = "Doe"
+        self.assertEqual(user.last_name, "Doe")
+
