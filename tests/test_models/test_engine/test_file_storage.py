1fdba4deeea2c13eab2d0c6d7759172a8d0260aa
Elchin <elcinceferli@gmail.com>
2024-03-01 21:22:56 +0400

    add test to filestorage save method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,13 @@
 import unittest
 from models import storage
 from models.base_model import BaseModel
+import json
 
 
 class TestFileStorage(unittest.TestCase):
     def test_save(self):
         instance = BaseModel()
         instance.save()
-        self.assertIn("BaseModel." + instance.id, storage.all())
+        with open("file.json", "r") as f:
+           data = json.load(f)
+           self.assertIn("BaseModel." + instance.id, data)
